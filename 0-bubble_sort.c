d3de57922d28266ac65eed57ff0091b6a0c6167c
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-12-15 11:03:41 +0100

    so help me God

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,25 +1,46 @@
 #include "sort.h"
+
 /**
- * bubble_sort - sort array lements from min to max value
- * @array: array
- * @size: array size
+ * swap_integers - Swap two integers in an array.
+ * @firstInteger: The first integer to swap.
+ * @secondInteger: The second integer to swap.
  */
-void bubble_sort(int *array, size_t size)
-{
+void swap_integers(int *firstInteger, int *secondInteger) {
+    int temporaryVariable;
 
-	size_t i, index, tmp = 0;
-
-	if (size < 2)
-		return;
-	for (i = 0; i < size; i++)
-		for (index = 0; index < size; index++)
-		{
-			if (array[index] > array[index + 1] && array[index + 1])
-			{
-			tmp = array[index];
-			array[index] = array[index + 1];
-			array[index + 1] = tmp;
-			print_array(array, size);
-			}
-		}
+    temporaryVariable = *firstInteger;
+    *firstInteger = *secondInteger;
+    *secondInteger = temporaryVariable;
 }
+
+/**
+ * bubble_sort - Sort an array of integers in ascending order.
+ * @listOfIntegers: An array of integers to sort.
+ * @numberOfIntegers: The size of the array.
+ *
+ * Description: Prints the array after each swap.
+ */
+void bubble_sort(int *listOfIntegers, size_t numberOfIntegers) {
+    size_t currentPosition, numberOfIntegersToSort;
+    bool swapsMade = false;
+
+    if (listOfIntegers == NULL || numberOfIntegers < 2) {
+        return;
+    }
+
+    while (!swapsMade) {
+        swapsMade = true;
+        numberOfIntegersToSort = numberOfIntegers - 1;
+
+        for (currentPosition = 0; currentPosition < numberOfIntegersToSort; currentPosition++) {
+            if (listOfIntegers[currentPosition] > listOfIntegers[currentPosition + 1]) {
+                swap_integers(&listOfIntegers[currentPosition], &listOfIntegers[currentPosition + 1]);
+                print_array(listOfIntegers, numberOfIntegers);
+                swapsMade = false;
+            }
+        }
+
+        numberOfIntegersToSort--;
+    }
+}
+
