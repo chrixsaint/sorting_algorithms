633ef2f55625faed82ddce637c1c64fbce293441
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-12-18 10:17:09 +0100

    103-merge_sort.c

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -6,23 +6,25 @@ void merge(int *array, int *new, int start, int m, int end);
 void print_array2(const int *array, size_t start, size_t end);
 
 /**
- * print_array2 - Customized print_array
- * @array: The array to be printed
- * @start: starting index
- * @end: ending index
- * Return: always 0
+ * print_array2 - Customized print_array function.
+ * @array: The array to be printed.
+ * @start: Starting index.
+ * @end: Ending index.
+ * Return: void
  */
 void print_array2(const int *array, size_t start, size_t end)
 {
-	size_t i;
+	size_t aii;
 
-	i = start;
-	while (array && i <= end)
+	aii = start;
+	while (array && aii <= end)
 	{
-		if (i > start)
+		if (aii > start)
+		{
 			printf(", ");
-		printf("%d", array[i]);
-		++i;
+		}
+		printf("%d", array[aii]);
+			++aii;
 	}
 	printf("\n");
 }
@@ -37,16 +39,18 @@ void print_array2(const int *array, size_t start, size_t end)
 
 void merge_sort(int *array, size_t size)
 {
-	int *new;
+	int *fresh;
 
 	if (size < 2 || !array)
+	{
 		return;
+	}
 
-	new = malloc(sizeof(int) * size);
+	fresh = malloc(sizeof(int) * size);
 
-	split(array, new, 0, size - 1);
+	split(array, fresh, 0, size - 1);
 
-	free(new);
+	free(fresh);
 }
 
 
@@ -62,7 +66,9 @@ int middle(int start, int end)
 	int middle = (end - start) / 2;
 
 	if ((end - start) % 2 == 0)
+	{
 		middle--;
+	}
 	return (middle + start);
 }
 
@@ -77,15 +83,15 @@ int middle(int start, int end)
 
 void split(int *array, int *new, int start, int end)
 {
-	int m;
+	int em;
 
 	if (start < end)
 	{
-		m = middle(start, end);
-		split(array, new, start, m);
-		split(array, new, m + 1, end);
+		em = middle(start, end);
+		split(array, new, start, em);
+		split(array, new, em + 1, end);
 
-		merge(array, new, start, m, end);
+		merge(array, new, start, em, end);
 	}
 
 
@@ -105,58 +111,58 @@ void merge(int *array, int *new, int start, int m, int end)
 {
 	int len_left = m - start + 1;
 	int len_right = end - m;
-	int l, r, i;
+	int l, r, aii;
 
 
 	printf("Merging...\n");
 	printf("[left]: ");
-	for (l = start, i = 0; l <= m; l++, i++)
+	for (l = start, aii = 0; l <= m; l++, aii++)
 	{
-		new[i] = array[l];
+		new[aii] = array[l];
 	}
 	print_array(new, len_left);
 
 	printf("[right]: ");
 
-	for (r = m + 1; r <= end; r++, i++)
+	for (r = m + 1; r <= end; r++, aii++)
 	{
-		new[i] = array[r];
+		new[aii] = array[r];
 	}
 
-	print_array2(new, len_left, i - 1);
+	print_array2(new, len_left, aii - 1);
 
 	l = 0;
 	r = len_left;
-	i = start;
+	aii = start;
 
 	while (l < len_left && r < len_right + len_left)
 	{
 		if (new[l] <= new[r])
 		{
-			array[i] = new[l];
+			array[aii] = new[l];
 			l++;
 		}
 		else
 		{
-			array[i] = new[r];
+			array[aii] = new[r];
 			r++;
 		}
-		i++;
+		aii++;
 	}
 
 
 	while (l < len_left)
 	{
-		array[i] = new[l];
+		array[aii] = new[l];
 		l++;
-		i++;
+		aii++;
 	}
 
 	while (r < len_right + len_left)
 	{
-		array[i] = new[r];
+		array[aii] = new[r];
 		r++;
-		i++;
+		aii++;
 	}
 
 	printf("[Done]: ");
